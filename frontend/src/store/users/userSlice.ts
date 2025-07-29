0d4761cef7da763ea51f0d71801e29964cf07923
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2025-07-29 07:41:44 +0530

    refactor: Remove unused import of PayloadAction in userSlice

--- frontend/src/store/users/userSlice.ts
+++ frontend/src/store/users/userSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from '@/store';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
